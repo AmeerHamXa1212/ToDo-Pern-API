63b5a580d9995b5cead2dcf06360a1154fdfa597
AmeerHamXa1212 <ameer.hamza@ocloudsolutions.net>
2023-09-05 17:34:07 +0500

    Project Setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const Port = process.env.PORT;
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(() => console.info(`Server is running at Port ${Port}`));
